46acaf737f0f4a5ed7c6d55635f3bb2811684335
Ayush0779 <159815989+Ayush0779@users.noreply.github.com>
2026-06-15 22:33:56 +0530

    Time: 5 ms (62.16%) | Memory: 44.4 MB (66.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,42 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> sMap=new HashMap<>();
-        Map<Character,Integer> tMap=new HashMap<>();
-
         if(s.length()!=t.length()){
-            
             return false;
         }
-
+        int[] chars=new int[26];
         for(int i=0;i<s.length();i++){
-            if(sMap.containsKey(s.charAt(i))){
-                sMap.put(s.charAt(i), sMap.get(s.charAt(i)) + 1);
-            }else{
-                sMap.put(s.charAt(i),1);
-            }
+            chars[s.charAt(i)-'a']++;
+            chars[t.charAt(i)-'a']--;
         }
 
-        for(int i=0;i<t.length();i++){
-            if(tMap.containsKey(t.charAt(i))){
-                tMap.put(t.charAt(i), tMap.get(t.charAt(i)) + 1);
-            }else{
-                tMap.put(t.charAt(i),1);
-            }
-        }
-
-        for (Character c : sMap.keySet()) {
-            if(!tMap.containsKey(c)){
-                
+        for(int i=0;i<chars.length;i++){
+            if(chars[i]!=0){
                 return false;
-            }else{
-                if(!sMap.get(c).equals(tMap.get(c))){
-                    System.out.println("hii");
-                    return false;
-                }
             }
         }
-
-
         return true;
 
     }
